edaeaae0d23b0bbcb04934900fdce8e2dad0978b
Alec Wydra <alec.wydra2@gmail.com>
2024-07-29 12:17:13 -0400

    Passed dependency graph to the front end

--- app/api/dependencyGraph/route.ts
+++ app/api/dependencyGraph/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function POST(request: NextRequest) {
     const data = await request.json();
     const code = data.code;
     const payload = {"source":code};
     try {
-        const response = await fetch('http://localhost:80/endpoint/', {
+        const response = await fetch('http://localhost:80/endpoint', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -13,13 +14,13 @@ export async function POST(request: NextRequest) {
             body: JSON.stringify(payload),
         });
         if (response.ok) {
-            const response_payload = await response.json();
+            var response_payload = await response.json();
             const edges = response_payload.links;
             const nodes = response_payload.nodes;
             console.log(edges)
             console.log(nodes)
         }
-        return NextResponse.json({ response: response});
+        return NextResponse.json({ response: response_payload});
     } catch (error) {
         console.error("Failed to connect to graph API: ",error);
         return NextResponse.json({
