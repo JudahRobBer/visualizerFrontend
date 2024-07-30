b82d2075f5e35103a3900b9c1fe732152d48aad9
JudahRobBer <143216510+JudahRobBer@users.noreply.github.com>
2024-07-30 16:12:33 -0400

    beginning to integrate reactflow

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import TaskCard from "./components/task/TaskCard";
 import CodeCard from "./components/task/CodeCard";
 import CodexCard from "./components/task/CodexCard";
 import CodeEditor from "./components/task/CodeEditor";
+import GraphCard from "./components/task/GraphCard";
 
 function Page() {
   const [code, setCode] = useState("");
@@ -20,7 +21,7 @@ function Page() {
 	<CodeEditor code={code} setCode={setCode}></CodeEditor>
       </div>
       <div className="basis-1/2">
-        {<CodeCard code={code} setCode={setCode}></CodeCard>}
+        {<GraphCard></GraphCard>}
       </div>
     </div>
   );
